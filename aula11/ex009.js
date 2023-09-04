83e142e249cce763b9ae7fcb020c4a8d5c048f4d
JacCarneiro <132837201+JacCarneiro@users.noreply.github.com>
2023-09-04 16:36:22 -0300

    resolvendo exercicios

--- aula11/ex009.js
+++ aula11/ex009.js
@@ -1,7 +1,10 @@
-var país = 'n'
+/*var país = 'n'
 console.log(`vivendo em ${país}`)
 if(país == 'Brasil'){
     console.log('Brasileiro!')
 }else{
     console.log('Estrangeiro!')
-}
+}*/
+
+var pais = 'mensagem'
+console.log(pais)
